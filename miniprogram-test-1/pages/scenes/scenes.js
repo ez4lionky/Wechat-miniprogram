0177a4c34b94f5f77199a44e970c2c93561f9116
ez4lionky <codex.lxy@gmail.com>
2019-05-09 21:05:21 +0800

    Images upload.

---
+++ miniprogram-test-1/pages/scenes/scenes.js
@@ -0,0 +1,81 @@
+// pages/scenes/scenes.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls: [
+      {
+        address: "../../images/paris.png",
+        description: "Select scene Paris"
+      },
+      {
+        address: '../../images/tengwang.png',
+        description: "Select scene TengWang pavilion"
+      },
+      {
+        address: '../../images/whitehouse.jpg',
+        description: "Select scene White House"
+      },
+      {
+        address: '../../images/palace.jpg',
+        description: "Select scene Palace"
+      }
+    ],
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
